e5443f119a2a64f8878e3429c813f6035a75c1d1
hss0818 <55937315+hss0818@users.noreply.github.com>
2022-04-20 23:59:35 -0500

    Update MCEM_helper.cpp

--- MCEM_helper.cpp
+++ MCEM_helper.cpp
@@ -5,9 +5,12 @@
 using namespace Rcpp;
 
 
-//' draw sample from categorical distribution with parameter p
+//' draw sample from categorical distribution 
 //' using built-in uniform distribution sampler
 //' by comparing the uniform sample with cumulative sum of p
+//' 
+//' @param P: categorical distribution parameter
+//' @return a sample from categorical distribution
 // [[Rcpp::export]]
 unsigned int rmultinomial(const arma::vec& P){
   double u = R::runif(0, sum(P));
@@ -22,8 +25,11 @@ unsigned int rmultinomial(const arma::vec& P){
 }
 
 
-//' draw sample from multinomial distribution with parameter (n,p)
+//' draw sample from multinomial distribution
 //' using categorical distribution sampler
+//'
+//' @param n, ps: multinomial distribution parameter
+//' @return a sample from multinomial distribution
 // [[Rcpp::export]]
 IntegerVector MultinomCalt(unsigned int n, arma::vec & ps) {
   IntegerVector res(ps.size());
@@ -35,8 +41,11 @@ IntegerVector MultinomCalt(unsigned int n, arma::vec & ps) {
 }
 
 
-//' draw sample from Dirichlet distribution with parameter deltas 
+//' draw sample from Dirichlet distribution
 //' using built-in Gamma distribution sampler
+//'
+//' @param deltas: Dirichlet distribution parameter
+//' @return a sample from Dirichlet distribution
 // [[Rcpp::export]]
 arma::vec rDirichlet(const arma::vec& deltas){
   unsigned int C = deltas.n_elem;
@@ -50,7 +59,15 @@ arma::vec rDirichlet(const arma::vec& deltas){
 }
 
 
-//' implementation of one iteration of Gibbs sampling within the E-step of the MCMC-EM algorithm
+//' implement one iteration of Gibbs sampling within the E-step of the MCMC-EM algorithm
+//' 
+//' @param D: term-doc matrix (V by d)
+//' @param C: current estimator of the topic matrix (V by k)
+//' @param W: Gibbs sample of W|Z from last iteration
+//' @param alpha: prior of columns of mixing matrix W
+//' @return C_hat: intermediate variable calculated from Gibbs sample of Z|X,C, 
+//' will be used for the estimation of the topic matrix
+//' @return W_hat: Gibbs sample of W|Z
 // [[Rcpp::export]]
 Rcpp::List one_it(const arma::sp_mat & D, const arma::mat& C, const arma::mat W, const arma::vec & alpha){
   unsigned int V = D.n_rows; // number of vocab
@@ -71,8 +88,8 @@ Rcpp::List one_it(const arma::sp_mat & D, const arma::mat& C, const arma::mat W,
     j = it.row(); // Row position: word index
     x =  *it;    // Value: word frequent
     arma::vec p = C.row(j).t()% W.col(i); // element-wise multiplication, multinomial parameter
-
-    // sample Z|C
+    
+    // sample Z|X,C
     if (x > 4){
       IntegerVector a = MultinomCalt(x, p);
       for (unsigned int l = 0; l < k; l++)
@@ -81,14 +98,14 @@ Rcpp::List one_it(const arma::sp_mat & D, const arma::mat& C, const arma::mat W,
         W_beta(l, i) = W_beta(l,i) + a(l);
       }
     }
-   else{
-     for (unsigned int l = 0; l < x; l++)
-     {
-       int a = rmultinomial(p) ;
-       C_hat(j, a) = C_hat(j,a) + 1;
-       W_beta(a, i) = W_beta(a,i) + 1;
-     }
-   }
+    else{
+      for (unsigned int l = 0; l < x; l++)
+      {
+        int a = rmultinomial(p) ;
+        C_hat(j, a) = C_hat(j,a) + 1;
+        W_beta(a, i) = W_beta(a,i) + 1;
+      }
+    }
   }
   
   // sample W|Z
@@ -102,20 +119,29 @@ Rcpp::List one_it(const arma::sp_mat & D, const arma::mat& C, const arma::mat W,
 }
 
 
-//' implementation of the E-step of the MCMC-EM Algorithm (Algorithm 1 in supplement material)
+//' implement the E-step of the MCMC-EM Algorithm (Algorithm 1 in supplement material)
+//' 
+//' @param C: current estimator of the topic matrix (V by k)
+//' @param D: term-doc matrix (V by d)
+//' @param W: randomly initialized mixing matrix W as the start point for Gibbs sampling
+//' @param alpha: prior of columns of mixing matrix W
+//' @param gibbs_steps: number of iterations in Gibbs sampling
+//' @param thin: keeps every thin sample in Gibbs sampling
+//' @return C_mean: raw estimator of the topic matrix C before column normalization
+//' @return W_mean: estimator of the mixing matrix W calculated from Gibbs samples of Z|W
 // [[Rcpp::export]]
-Rcpp::List E_step(arma::mat & C, const arma::sp_mat & D, arma::mat  W,
+Rcpp::List E_step(arma::mat & C, const arma::sp_mat & D, arma::mat W,
                   const arma::vec & alpha, unsigned int gibbs_steps, unsigned int thin,
                   double burn_in){
   unsigned int V = D.n_rows; // number of vocab
   unsigned int d = D.n_cols; // number of docs
   unsigned int k = C.n_cols; // number of topics
   double count = 0.0; // count of number of Gibbs samples
- 
+  
   // initialize cumulative sum for matrix from Gibbs sampling
   arma::mat C_cul = arma::zeros<arma::mat>(V, k);
   arma::mat W_cul = arma::zeros<arma::mat>(k, d);
-
+  
   // Gibbs sampling
   for (unsigned int t = 0; t < gibbs_steps; t++){
     List L = one_it(D, C, W, alpha); // call function to implement one iteration
@@ -141,6 +167,12 @@ Rcpp::List E_step(arma::mat & C, const arma::sp_mat & D, arma::mat  W,
 
 
 //' calculate the log-likelihood 
+//' 
+//' @param D: term-doc matrix (V by d)
+//' @param C: estimator of the topic matrix (V by k)
+//' @param W: estimator of the mixing matrix (k by d)
+//' @param thr: to avoid numeric issue when take log
+//' @return lkh: the log-likelihood 
 // [[Rcpp::export]]
 double lkh_cal(const arma::sp_mat & D, const arma::mat& C, const arma::mat& W, const double thr=1e-9){
   unsigned int i;
@@ -148,7 +180,6 @@ double lkh_cal(const arma::sp_mat & D, const arma::mat& C, const arma::mat& W, c
   unsigned int x;
   double lkh;
   lkh = 0.0;
-  
   
   // iterate over all elements of D
   for(arma::sp_mat::const_iterator it = D.begin(); it != D.end(); ++it){
